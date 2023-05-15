990281293a5b7189bfaa376cb591479fdaaa7cb3
atakanbeddall <atakan.beddall@gmail.com>
2023-05-15 20:18:58 +0100

    Added error decorator

---
+++ src/common/response-decorator/errorDecorator.ts
@@ -0,0 +1,15 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  constructor() {}
+
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+
+    response.status(status).json({ statusCode: status, status: 'fail', message: exception['response'].message });
+  }
+}
